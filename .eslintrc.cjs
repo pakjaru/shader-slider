8ff9cd65efba4a053e4ca18d9adbf916c44fc5a2
pakjaru <fajarudewangga@gmail.com>
2024-04-21 22:23:15 +0800

    setup tailwindcss

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,10 +6,11 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
+    'plugin:tailwindcss/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'tailwindcss'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
